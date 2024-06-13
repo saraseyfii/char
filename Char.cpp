15b459637149bef207bd506021744c141b352bca
saraseyfii <seyfisara68@gmail.com>
2024-06-13 12:32:39 +0330

    Add files via upload

---
+++ Char.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+{
+	char c;
+	cin >> c;
+	if (c >= '0' && c <= '9') {
+		cout << "it is pigit";
+	}
+	else {
+		cout << "bal in put!";
+	}
+	return 0;
+}
